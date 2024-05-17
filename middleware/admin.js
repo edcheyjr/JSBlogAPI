6ff4cb4fd4fccee6a4dc4975513d11ae44dfa2df
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2024-05-17 15:33:34 +0300

    admin fixed logic

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
+import { logMessage, logObjectData } from '../utils/logging.js'
 
 const verifyAdmin = (req, res, next) => {
   const tokenSecret = process.env.JWT_TOKEN_SECRET || 'my-token-secret'
@@ -7,15 +8,22 @@ const verifyAdmin = (req, res, next) => {
   const token = req.headers.authorization
   if (!token) res.status(403).json({ error: 'please provide a token' })
   else {
+    logMessage(`verifying token: ${token}`)
     jwt.verify(token.split(' ')[1], tokenSecret, async (err, value) => {
-      if (err) res.status(500).json({ error: 'failed to authenticate token' })
-      req.user = value.data
-      const user = await User.findOne({ email: req.user })
-      console.log(user)
-      if (user.role != 1) {
-        res.status(403).send({ error: 'not authorized to do this operations' })
+      if (err) {
+        logObjectData(err)
+        res.status(500).json({ error: 'failed to authenticate token' })
+      } else {
+        req.user = value.data
+        const user = await User.findOne({ email: req.user })
+        console.log(user)
+        if (user.role != 1) {
+          res
+            .status(403)
+            .send({ error: 'not authorized to do this operations' })
+        }
+        next()
       }
-      next()
     })
   }
 }
