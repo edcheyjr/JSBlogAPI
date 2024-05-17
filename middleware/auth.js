08f0481835969a17d992f18677fe07eb065d066a
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2024-05-17 15:59:22 +0300

    auth middleware update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,21 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 
-const tokenSecret = 'my-token-secret';
+const tokenSecret = 'my-token-secret'
 
 const verify = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) res.status(403).json({ error: 'please provide a token' });
+  const token = req.headers.authorization
+  if (!token) res.status(403).json({ error: 'please provide a token' })
   else {
+    logMessage(`verifying token: ${token}`)
     jwt.verify(token.split(' ')[1], tokenSecret, (err, value) => {
-      if (err) res.status(500).json({ error: 'failed to authenticate token' });
-      req.user = value.data;
-      next();
-    });
+      if (err) {
+        logObjectData(err)
+        res.status(500).json({ error: 'failed to authenticate token' })
+      } else {
+        req.user = value.data
+        next()
+      }
+    })
   }
-};
-export default verify;
+}
+export default verify
